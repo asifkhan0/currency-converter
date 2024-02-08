6f711f98d3015aa820c05101f14f53de4d1875ce
asifkhan0 <ashifkhan321.ak8@gmail.com>
2024-02-08 23:59:43 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./App.css";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
+
+function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+  };
+
+  return (
+    <>
+      <h1 className="text-3xl bg-orange-500">Currency converter app</h1>
+    </>
+  );
+}
+
+export default App;
